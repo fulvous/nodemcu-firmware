e375f7bcbecfe70cf51a43b1b92684597d23e20c
León Ramos <leon.ramos@gmail.com>
2016-03-21 23:03:23 -0600

    Customized modules and tagged version

--- app/include/user_version.h
+++ app/include/user_version.h
@@ -6,9 +6,9 @@
 #define NODE_VERSION_REVISION	1U
 #define NODE_VERSION_INTERNAL   0U
 
-#define NODE_VERSION	"NodeMCU 1.5.1"
+#define NODE_VERSION	"NodeMCU 1.5.1 Farmtomatic"
 #ifndef BUILD_DATE
-#define BUILD_DATE	  "unspecified"
+#define BUILD_DATE	  "20160321"
 #endif
 
 extern char SDK_VERSION[];
